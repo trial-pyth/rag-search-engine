ec3e7dc2e59b23485c3938e3265da69fd7652493
trial-pyth <smigr1279@gmail.com>
2026-02-28 06:01:05 +0000

    loading, creating and saving TF

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,5 +1,57 @@
+import pickle
+import os
 import string
-from lib.search_utils import load_movies, load_stopwords
+from lib.search_utils import load_movies, load_stopwords, CACHE_PATH 
+from nltk.stem import PorterStemmer
+from collections import defaultdict
+
+stemmer = PorterStemmer()
+
+class InvertedIndex:
+    def __init__(self):
+        self.index = defaultdict(set) 
+        self.docmap = {} # map document ID to document
+        self.index_path = CACHE_PATH / 'index.pkl'
+        self.docmap_path = CACHE_PATH / 'docmap.pkl'
+
+    def __add_document(self, doc_id, text):
+        tokens = tokenize_text(text)
+        for token in set(tokens):
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term):
+        return sorted(list(self.index[term]))
+
+    def build(self):
+        movies = load_movies()
+        for movie in movies:
+            doc_id = movie['id']
+            text = f"{movie['title']} {movie['description']}" 
+            self.__add_document(doc_id, text)
+            self.docmap[doc_id] = movie
+    
+    def save(self):
+        os.makedirs(CACHE_PATH, exist_ok=True)
+        with open( self.index_path, 'wb') as f:
+            pickle.dump(self.index, f)
+        with open(self.docmap_path, 'wb') as f:
+            pickle.dump(self.docmap, f)
+
+    def load(self):
+        with open(self.index_path, 'rb') as f:
+            self.index = pickle.load( f)
+        with open(self.docmap_path, 'rb') as f:
+            self.docmap = pickle.load( f)
+        
+
+
+def build_command():
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+    docs = idx.get_documents("merida")
+    print(f"first document for token 'merida' = {docs[0]} ")
+
 
 def clean_text(text):
     text=  text.lower()
@@ -16,8 +68,10 @@ def tokenize_text(text):
         if tok and tok not in  stopwords:
             return True
         return False
+        
     for tok in text.split():
         if _filter(tok):
+            tok = stemmer.stem(tok)
             res.append(tok)
     return res
 
@@ -28,10 +82,26 @@ def has_matching_token(query_toks, movie_toks):
                 return True
     return False
 
-def search_command(query, n_results):
+def search_command(query, n_results = 5):
     movies = load_movies()
-    res = []
+    idx = InvertedIndex()
+    idx.load()
+    seen, res = set(), []
     query_tokens = tokenize_text(query)
+    for qt in query_tokens:
+        matching_docs_ids = idx.get_documents(qt)
+        for matchin_doc_id in matching_docs_ids:
+            if matchin_doc_id in seen:
+                continue
+            seen.add(matchin_doc_id)
+            matching_doc = idx.docmap[matchin_doc_id]
+            res.append(matching_doc)
+
+            if len(res) >= n_results:
+                return res
+    return res
+
+        
     for movie in movies:
         movie_tokens = tokenize_text(movie['title'])
         if has_matching_token(query_tokens, movie_tokens):
--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -2,17 +2,18 @@ import json
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
-DATA_PATH = PROJECT_ROOT/'data'
-MOVIES_PATH = DATA_PATH/'movies.json'
-STOPWORDS_PATH = DATA_PATH/'stopwords.txt'
+DATA_PATH = PROJECT_ROOT / 'data'
+MOVIES_PATH = DATA_PATH / 'movies.json'
+STOPWORDS_PATH = DATA_PATH / 'stopwords.txt'
 
+CACHE_PATH = PROJECT_ROOT / 'cache'
 
 def load_movies() -> list[dict]:
     with open(MOVIES_PATH, "r") as f:
         data = json.load(f)
-        return data['movies']
+    return data['movies']
 
 def load_stopwords():
     with open(STOPWORDS_PATH, 'r') as f:
         data = f.read().splitlines()
-    return data
+    return data 
