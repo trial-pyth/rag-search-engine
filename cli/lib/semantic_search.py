fabe6c9621c1f2358b2046faa6b47a06294940db
trial-pyth <smigr1279@gmail.com>
2026-03-01 11:09:23 +0000

    Document embedings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,15 +1,44 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
+from pathlib import Path
+from search_utils import load_movies
 
 class SemanticSearch:
     def __init__(self):
         self.model = SentenceTransformer('all-MiniLLM-L6-v2')
-    
+        self.embeddings = None 
+        self.documents = None
+        self.document_map = {}
+        self.embeddings_path = Path("cache/movie_embeddings.npy")
+
+    def build_embeddings(self, documents):
+        self.documents = documents
+        self.document_map = {}
+        movie_strings = []
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc
+            movie_strings.append(f"{doc['title']}: {doc['description']}")
+        self.embeddings =  self.model.encode(movie_strings, show_progress_bar=True)
+        np.save(self.embeddings_path, self.embeddings)
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        self.document_map = {}
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc
+        if self.embeddings_path.exists():
+            self.embeddings = np.load(self.embeddings_path)
+            if len(self.documents) == len(self.embeddings):
+                return self.embeddings
+        return self.build_embeddings(documents)    
+
+
     def generate_embedding(self, text):
         if not text or not text.string():
             raise ValueError("Must have text to create an embedding")
         return self.model.encode([text])[0]
  
-
 def embed_text(text):
     ss = SemanticSearch()
     embedding = ss.generate_embedding(text)
@@ -21,3 +50,11 @@ def verify_model():
     ss = SemanticSearch()
     print(f"Model loaded: {ss.model}")
     print(f"max sequence length: {ss.model.max_seq_length}")
+
+
+def verify_embeddings(documents):
+    ss =SemanticSearch()
+    documents = load_movies()
+    embeddings = ss.load_or_create_embeddings(documents)
+    print(f"Number of docs:   {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
